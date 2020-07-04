b1bf4cbcb5175b0b3f531382142cec9def5b590c
Nicolas Kiely <mitchell3141@gmail.com>
2020-07-04 01:43:56 -0400

    Register models with admin site

--- channel/admin.py
+++ channel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from channel import models
+
 # Register your models here.
+admin.site.register(models.Channel)
+admin.site.register(models.ChannelMember)
--- notification/admin.py
+++ notification/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from notification import models
+
+
+admin.site.register(models.Notification)
